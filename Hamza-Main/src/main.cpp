63edd565a2f009d58e9236fc4a3356d7145da390
ahkad-laptop <ahkad@laptop.com>
2023-01-04 01:22:16 +0300

    all relay added mqtt json

--- Hamza-Main/src/main.cpp
+++ Hamza-Main/src/main.cpp
@@ -72,222 +72,6 @@ boolean bluetoothState = false;
   Serial.println(WiFi.localIP());
 }
 
-// void bluetoothIR()
-// {
-// <<<<<<< HEAD
-//   // irsend.sendRaw(rawData, 71, 32); // Send a raw data capture at 38kHz.
-//   // irsend.sendNEC(0xFF12ED, 32);   //Power
-//   // delay(50);
-//   irsend.sendNEC(0xFF12ED, 32, 40);
-  
-// =======
-//   irsend.sendNEC(0xFF12ED, 32, 40); // Send a raw data capture at 38kHz.
-// >>>>>>> e6e5d22f431ca0b017324e8ff174d3cb6eb5c3c2
-// }
-// void bluetoothON()
-// {
-//   if (bluetoothState == false)
-//   {
-//     bluetoothIR();
-//     bluetoothState = true;
-//   }
-// }
-// void bluetoothOFF()
-// {
-//   if (bluetoothState == true)
-//   {
-//     bluetoothIR();
-//     bluetoothState = false;
-    
-//   }
-// }
-
-// BLYNK_WRITE(V0)
-// {
-//   switch (param.asInt())
-//   {
-//   case 1: // Main speakers ON
-//     bluetoothON();
-//     digitalWrite(BathAMP, HIGH);
-//     digitalWrite(BathSpeakers, HIGH);
-//     bridgeBedroom.virtualWrite(V1, 0);
-//     bridgeBedroom.virtualWrite(V2, 0);
-//     delay(100);
-//     digitalWrite(MainAMP, LOW);
-//     digitalWrite(MainSpeakers, LOW);
-//     break;
-//   case 2: // Bathroom speakers ON
-//     bluetoothON();
-//     digitalWrite(MainAMP, HIGH);
-//     digitalWrite(MainSpeakers, HIGH);
-//     bridgeBedroom.virtualWrite(V1, 0);
-//     bridgeBedroom.virtualWrite(V2, 0);
-//     delay(100);
-//     digitalWrite(BathAMP, LOW);
-//     digitalWrite(BathSpeakers, LOW);
-//     break;
-//   case 3: // Bedroom speakers ON
-//     bluetoothON();
-//     digitalWrite(MainAMP, HIGH);
-//     digitalWrite(BathAMP, HIGH);
-//     digitalWrite(MainSpeakers, HIGH);
-//     digitalWrite(BathSpeakers, HIGH);
-//     delay(100);
-//     bridgeBedroom.virtualWrite(V1, 1);
-//     bridgeBedroom.virtualWrite(V2, 1);
-//     break;
-//   case 4: // All ON
-//     bluetoothON();
-//     digitalWrite(MainAMP, LOW);
-//     digitalWrite(BathAMP, LOW);
-//     digitalWrite(MainSpeakers, LOW);
-//     digitalWrite(BathSpeakers, LOW);
-//     bridgeBedroom.virtualWrite(V1, 1);
-//     bridgeBedroom.virtualWrite(V2, 1);
-//     break;
-//   case 5: // All OFF
-//     bluetoothOFF();
-//     digitalWrite(MainAMP, HIGH);
-//     digitalWrite(BathAMP, HIGH);
-//     digitalWrite(MainSpeakers, HIGH);
-//     digitalWrite(BathSpeakers, HIGH);
-//     bridgeBedroom.virtualWrite(V1, 0);
-//     bridgeBedroom.virtualWrite(V2, 0);
-//     break;
-//   default:
-//     break;
-//   }
-// }
-
-// BLYNK_WRITE(V1) // Switch between front and roof's speakers in Hall
-// {
-//   frontState = param.asInt();
-//   digitalWrite(FrontSpeakers, !frontState);
-// }
-
-// BLYNK_WRITE(V2) // Manual control Entery lights - light around house
-// {
-//   outsideState = param.asInt();
-//   bridgeEntery.virtualWrite(V2, outsideState);
-// }
-// BLYNK_WRITE(V3) // Manual control Entery lights - Garage lights
-// {
-//   enteryState = param.asInt();
-//   bridgeEntery.virtualWrite(V1, enteryState);
-// }
-
-// BLYNK_WRITE(V4) // Time Enable on Entery board
-// {
-//   bridgeEntery.virtualWrite(V6, param.asInt());
-// }
-
-// BLYNK_WRITE(V5) // Set Hold IR blutooth's power button
-// {
-//   holdBtnTime = param.asInt();
-// }
-
-// BLYNK_WRITE(V11)
-// { // Wireless Switch 1
-//   lightSwitch01 = param.asInt();
-//   bridgeEntery.virtualWrite(V11, lightSwitch01);
-// }
-
-// BLYNK_WRITE(V12)
-// { // Wireless Switch 1
-//   lightSwitch02 = param.asInt();
-//   bridgeEntery.virtualWrite(V12, lightSwitch02);
-// }
-// BLYNK_WRITE(V13) // Wireless Switch 3
-// {
-//   lightSwitch03 = param.asInt();
-//   bridgeEntery.virtualWrite(V13, lightSwitch03);
-// }
-// BLYNK_WRITE(V14) // Wireless Switch 4
-// {
-//   lightSwitch04 = param.asInt();
-//   bridgeEntery.virtualWrite(V14, lightSwitch04);
-// }
-// BLYNK_WRITE(V15) // Wireless Switch 5
-// {
-//   lightSwitch05 = param.asInt();
-//   bridgeEntery.virtualWrite(V15, lightSwitch05);
-// }
-// BLYNK_WRITE(V16) // Wireless Switch 6
-// {
-//   lightSwitch06 = param.asInt();
-//   bridgeEntery.virtualWrite(V16, lightSwitch06);
-// }
-// BLYNK_WRITE(V17) // Wireless Switch 7
-// {
-//   lightSwitch07 = param.asInt();
-//   bridgeEntery.virtualWrite(V17, lightSwitch07);
-// }
-// BLYNK_WRITE(V18) // Wireless Switch 8
-// {
-//   lightSwitch08 = param.asInt();
-//   bridgeEntery.virtualWrite(V18, lightSwitch08);
-// }
-// BLYNK_WRITE(V19) // Wireless Switch 9
-// {
-//   lightSwitch09 = param.asInt();
-//   bridgeEntery.virtualWrite(V19, lightSwitch09);
-// }
-
-// BLYNK_WRITE(V20) // Wireless Switch 9
-// {
-//   bluetoothButton = param.asInt();
-//   if( bluetoothButton == 1){
-//     bluetoothON();
-//   }
-//   else if(bluetoothButton == 0){
-//     bluetoothOFF();
-//   }
-// }
-
-// BLYNK_WRITE(V21)
-// { // Wireless Switch 9
-//   lightSwitch21 = param.asInt();
-//   if (lightSwitch09 == 1)
-//   {
-//     mySwitch.send(3158590, 24);
-//   }
-//   else
-//   {
-//     mySwitch.send(3159494, 24);
-//   }
-// }
-// BLYNK_WRITE(V22)
-// { // Wireless Switch 9
-//   lightSwitch22 = param.asInt();
-//   if (lightSwitch09 == 1)
-//   {
-//     mySwitch.send(3158530, 24);
-//   }
-//   else
-//   {
-//     mySwitch.send(3159414, 24);
-//   }
-// }
-// BLYNK_WRITE(V23)
-// { // Wireless Switch 9
-//   lightSwitch23 = param.asInt();
-//   if (lightSwitch09 == 1)
-//   {
-//     mySwitch.send(3159355, 24);
-//   }
-//   else
-//   {
-//     mySwitch.send(3159194, 24);
-//   }
-// }
-// // This function is called every time the device is connected to the Blynk.Cloud
-// BLYNK_CONNECTED()
-// {
-//   bridgeBedroom.setAuthToken(BedroomAUTH);
-//   bridgeEntery.setAuthToken(EnteryAUTH);
-// }
-
-
 void callback(char *topic, byte *payload, unsigned int length)
 {
   Serial.print("Message arrived [");
@@ -333,29 +117,121 @@ void callback(char *topic, byte *payload, unsigned int length)
   case 2:
     if (valuejson == 1)
     {
-       
+      digitalWrite(MainSpeakers, HIGH);
+      client.publish("MainSpeakers", "ON");
     }
     else if (valuejson == 0)
     {
-      
+      digitalWrite(MainSpeakers, LOW);
+      client.publish("MainSpeakers", "OFF");
     }
     break;
 
   case 3:
-    if (valuejson == 1) // Rolling Screen UP
+    if (valuejson == 1)
     {
-       
-    }
-    else if (valuejson == 2) // Rolling Screen UP
-    {
-       
+      digitalWrite(BathSpeakers, HIGH);
+      client.publish("BathSpeakers", "ON");
     }
     else if (valuejson == 0)
     {
-      
+      digitalWrite(BathSpeakers, LOW);
+      client.publish("BathSpeakers", "OFF");
     }
     break;
 
+    case 4:
+    if (valuejson == 1)
+    {
+      digitalWrite(FrontSpeakers, HIGH);
+      client.publish("FrontSpeakers", "ON");
+    }
+    else if (valuejson == 0)
+    {
+      digitalWrite(FrontSpeakers, LOW);
+      client.publish("FrontSpeakers", "OFF");
+    }
+    break;
+
+    case 5:
+    if (valuejson == 1)
+    {
+      digitalWrite(MainAMP, HIGH);
+      client.publish("MainAMP", "ON");
+    }
+    else if (valuejson == 0)
+    {
+      digitalWrite(MainAMP, LOW);
+      client.publish("MainAMP", "OFF");
+    }
+    break;
+
+    case 6:
+    if (valuejson == 1)
+    {
+      digitalWrite(BathAMP, HIGH);
+      client.publish("BathAMP", "ON");
+    }
+    else if (valuejson == 0)
+    {
+      digitalWrite(BathAMP, LOW);
+      client.publish("BathAMP", "OFF");
+    }
+    break;
+
+    case 7:
+    if (valuejson == 1)
+    {
+      mySwitch.send(3158530, 24);
+      client.publish("hamza-light1", "ON");
+    }
+    else if (valuejson == 0)
+    {
+      mySwitch.send(3159414, 24);
+      client.publish("hamza-light1", "OFF");
+    }
+    break;
+
+    case 8:
+    if (valuejson == 1)
+    {
+      mySwitch.send(3158590, 24);
+      client.publish("hamza-light2", "ON");
+    }
+    else if (valuejson == 0)
+    {
+      mySwitch.send(3159494, 24);
+      client.publish("hamza-light2", "OFF");
+    }
+    break;
+
+    case 9:
+    if (valuejson == 1)
+    {
+      mySwitch.send(3158532, 24);
+      client.publish("hamza-light3", "ON");
+    }
+    else if (valuejson == 0)
+    {
+      mySwitch.send(3159413, 24);
+      client.publish("hamza-light3", "OFF");
+    }
+    break;
+
+    case 0:
+    if (valuejson == 1)
+    {
+      irsend.sendNEC(0xFF12ED, 32, 40);
+      client.publish("hamza-blutooth", "ON40");
+    }
+    else if (valuejson == 0)
+    {
+       irsend.sendNEC(0xFF12ED, 32, 80);
+       client.publish("hamza-blutooth", "ON80");
+    }
+    break;
+
+     
   default:
     Serial.print("Err device in case-switch invalid.");
     break;
