5a07dc7b41434cb7e9422ba73bb9fe09a30c5b01
ahkad-laptop <ahkad@laptop.com>
2023-01-04 01:39:42 +0300

    moved to home assistant

--- Hamza-Entery/src/main.cpp
+++ Hamza-Entery/src/main.cpp
@@ -3,230 +3,193 @@
 #include "secret.h"   //Wifi and blynk server info
 
 #include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#include <TimeLib.h>
-#include <WidgetRTC.h>
+#include <PubSubClient.h> //mqtt lib
+
+#include <ArduinoJson.h>
+
+#include <ArduinoOTA.h>
 
 #include <RCSwitch.h> //for wireless RF433 control
 RCSwitch mySwitch = RCSwitch();
 
-// Comment this out to disable prints and save space
-#define BLYNK_PRINT Serial
+const char *ssid = "Ahmedov" ;       // defined on secret.h
+const char *password = "Ahmedov90";   // defined on secret.h
+const char *mqtt_server = mqttURL; // defined on secret.h
+const char *deviceName = "Hamza-Entry";
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-BlynkTimer timer;
+StaticJsonDocument<100> doc;
 
-WidgetRTC rtc;
-WidgetLED led(V5);
+int device;
+int valuejson;
 
 
-char auth[] = EnteryAUTH;
-// Your WiFi credentials.
-char ssid[] = WIFI_SSID;
-char pass[] = WIFI_PASS;
-int EnteryState;
-int OutsideState;
-int secondsNow;
-int timerEnable = 1;
-int setON = 64800;  // default time on is 18:00
-int setOFF = 19800; // default time off is 05:30
+ void setup_wifi() {
 
-int lightSwitch01;
-int lightSwitch02;
-int lightSwitch03;
-int lightSwitch04;
-int lightSwitch05;
-int lightSwitch06;
-int lightSwitch07;
-int lightSwitch08;
-int lightSwitch09;
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-BLYNK_WRITE(V1)
-{
-  if (timerEnable == 0)
-  {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
-    EnteryState = param.asInt();
-    digitalWrite(EnteryLight, !EnteryState);
-    Serial.print("EnteryState = ");
-    Serial.println(EnteryState);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
-BLYNK_WRITE(V2)
+
+void callback(char *topic, byte *payload, unsigned int length)
 {
-  if (timerEnable == 0)
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (unsigned int i = 0; i < length; i++)
   {
-    OutsideState = param.asInt();
-    digitalWrite(OutsideLight, !OutsideState);
+    Serial.print((char)payload[i]);
   }
-}
+  Serial.println();
 
-BLYNK_WRITE(V3)
-{
-  setON = param.asInt();
-}
-
-BLYNK_WRITE(V4)
-{
-  setOFF = param.asInt();
-}
-
-BLYNK_WRITE(V6)
-{
-  timerEnable = param.asInt();
-}
-
-void outDoorLights()
-{
-  secondsNow = (hour() * 3600) + (minute() * 60) + second(); // count seconds to compare it with setON and setOFF
-  if (timerEnable == 1)
+  // Test if parsing succeeds.
+  DeserializationError error = deserializeJson(doc, payload);
+  if (error)
   {
-    if (secondsNow >= setON && secondsNow < setOFF)
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }
+  // Print the values to data types
+  device = doc["device"].as<unsigned int>();
+  valuejson = doc["value"].as<unsigned int>();
+  Serial.print("device: ");
+  Serial.print(device);
+  Serial.print(" valuejson: ");
+  Serial.println(valuejson);
+
+  switch (device)
+  {
+  case 1:
+    if (valuejson == 1)
+    {
+      digitalWrite(EnteryLight, HIGH);
+      client.publish("EnteryLight", "ON");
+    }
+    else if (valuejson == 0)
+    {
+      digitalWrite(EnteryLight, LOW);
+      client.publish("EnteryLight", "OFF");
+    }
+    break;
+
+  case 2:
+    if (valuejson == 1)
+    {
+      digitalWrite(OutsideLight, HIGH);
+      client.publish("OutsideLight", "ON");
+    }
+    else if (valuejson == 0)
     {
       digitalWrite(OutsideLight, LOW);
-      led.on();
+      client.publish("OutsideLight", "OFF");
+    }
+    break;
+
+  case 3:
+    if (valuejson == 1)
+    {
+      mySwitch.send(3138542, 24);
+      client.publish("hamza-light4", "ON");
+    }
+    else if (valuejson == 0)
+    {
+      mySwitch.send(3119513, 24);
+      client.publish("hamza-light4", "OFF");
+    }
+    break;
+
+    case 4:
+    if (valuejson == 1)
+    {
+      mySwitch.send(3128232, 24);
+      client.publish("hamza-light5", "ON");
+    }
+    else if (valuejson == 0)
+    {
+      mySwitch.send(3188413, 24);
+      client.publish("hamza-light5", "OFF");
+    }
+    break;
+ 
+
+     
+  default:
+    Serial.print("Err device in case-switch invalid.");
+    break;
+  }
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(mqttClient, mqttName, mqttPASS))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("LifeTopic", "{\"device\":\"Hamza-Entry\"}");
+      // ... and resubscribe
+      client.subscribe("Hamza-Entry");
     }
     else
     {
-
-      digitalWrite(OutsideLight, HIGH);
-      led.off();
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
   }
-}
-
-BLYNK_WRITE(V11)
-{ // Wireless Switch 1
-  lightSwitch01 = param.asInt();
-  if (lightSwitch01 == 1)
-  {
-    mySwitch.send(3159510, 24);
-  }
-  else
-  {
-    mySwitch.send(3159514, 24);
-  }
-}
-BLYNK_WRITE(V12)
-{ // Wireless Switch 2
-  lightSwitch02 = param.asInt();
-  if (lightSwitch02 == 1)
-  {
-    mySwitch.send(3159520, 24);
-  }
-  else
-  {
-    mySwitch.send(3159524, 24);
-  }
-}
-BLYNK_WRITE(V13)
-{ // Wireless Switch 3
-  lightSwitch03 = param.asInt();
-  if (lightSwitch03 == 1)
-  {
-    mySwitch.send(3159530, 24);
-  }
-  else
-  {
-    mySwitch.send(3159534, 24);
-  }
-}
-BLYNK_WRITE(V14)
-{ // Wireless Switch 4
-  lightSwitch04 = param.asInt();
-  if (lightSwitch04 == 1)
-  {
-    mySwitch.send(3159540, 24);
-  }
-  else
-  {
-    mySwitch.send(3159544, 24);
-  }
-}
-BLYNK_WRITE(V15)
-{ // Wireless Switch 5
-  lightSwitch05 = param.asInt();
-  if (lightSwitch05 == 1)
-  {
-    mySwitch.send(3159550, 24);
-  }
-  else
-  {
-    mySwitch.send(3159554, 24);
-  }
-}
-BLYNK_WRITE(V16)
-{ // Wireless Switch 6
-  lightSwitch06 = param.asInt();
-  if (lightSwitch06 == 1)
-  {
-    mySwitch.send(3159560, 24);
-  }
-  else
-  {
-    mySwitch.send(3159564, 24);
-  }
-}
-BLYNK_WRITE(V17)
-{ // Wireless Switch 7
-  lightSwitch07 = param.asInt();
-  if (lightSwitch07 == 1)
-  {
-    mySwitch.send(3159570, 24);
-  }
-  else
-  {
-    mySwitch.send(3159574, 24);
-  }
-}
-BLYNK_WRITE(V18)
-{ // Wireless Switch 8
-  lightSwitch08 = param.asInt();
-  if (lightSwitch08 == 1)
-  {
-    mySwitch.send(3159580, 24);
-  }
-  else
-  {
-    mySwitch.send(3159584, 24);
-  }
-}
-BLYNK_WRITE(V19)
-{ // Wireless Switch 9
-  lightSwitch09 = param.asInt();
-  if (lightSwitch09 == 1)
-  {
-    mySwitch.send(3159590, 24);
-  }
-  else
-  {
-    mySwitch.send(3159594, 24);
-  }
-}
-// This function is called every time the device is connected to the Blynk.Cloud
-BLYNK_CONNECTED()
-{
-  // Synchronize time on connection
-  rtc.begin();
 }
 
 void setup()
 {
-  // Debug console
-  Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
-  mySwitch.enableTransmit(RFPin); // GPIO15 D7 RF radio pin
-
   pinMode(EnteryLight, OUTPUT);
   pinMode(OutsideLight, OUTPUT);
 
   digitalWrite(EnteryLight, HIGH); // all relays off when startup
   digitalWrite(OutsideLight, HIGH);
 
-  timer.setInterval(10000L, outDoorLights);
+  Serial.begin(115200); // debug print on Serial Monitor
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  
+  mySwitch.enableTransmit(RFPin); // GPIO15 D7 RF radio pin
+ ArduinoOTA.setHostname(mqttClient);
+  ArduinoOTA.begin();
 }
 
 void loop()
 {
-  Blynk.run();
-  timer.run();
+  ArduinoOTA.handle();
+
+  if (!client.connected())
+  {
+    Serial.println("reconnecting ...");
+    reconnect();
+  }
+  client.loop();
 }
